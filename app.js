40e7a0d0f075de9088ec41b59c70b018ba3227a3
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-07-15 11:00:01 -0700

    first draft

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-// import functions and grab DOM elements
+// grab DOM elements
+const h1El = document.getElementById('animal-name');
+const horseEl = document.getElementById('horse-image');
+const dogEl = document.getElementById('dog-image');
+const catEl = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
 
-// initialize state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+horseEl.addEventListener('click', () => {
+    horseSound.play();
+    h1El.textContent = 'horse';
+});
+
+dogEl.addEventListener('click', () => {
+    dogSound.play();
+    h1El.textContent = 'dog';
+});
+
+catEl.addEventListener('click', () => {
+    catSound.play();
+    h1El.textContent = 'cat';
+
+});
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'c') {
+        catSound.play();
+    }
+    if (event.key === 'd') {
+        dogSound.play();
+    }
+    if (event.key === 'h') {
+        horseSound.play();
+    }
+});
