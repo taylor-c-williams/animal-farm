9638a5537a6522348b1f051f2d1c1ebbffccea64
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-07-15 11:58:37 -0700

    lisa frank backgrounds

--- app.js
+++ app.js
@@ -11,17 +11,20 @@ const catSound = document.getElementById('cat-sound');
 // set event listeners 
 horseEl.addEventListener('click', () => {
     horseSound.play();
-    h1El.textContent = 'horse';
+    h1El.textContent = 'horsey';
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/e6/3f/ac/e63fac00637cce9fb90d67683d3b5e9c.jpg') ";
 });
 
 dogEl.addEventListener('click', () => {
     dogSound.play();
-    h1El.textContent = 'dog';
+    h1El.textContent = 'doggie';
+    document.body.style.backgroundImage = "url('https://i0.wp.com/dailydot.com/wp-content/uploads/692/91/5f3554c793183fa4176cd2de936c83c5.jpg') ";
 });
 
 catEl.addEventListener('click', () => {
     catSound.play();
-    h1El.textContent = 'cat';
+    h1El.textContent = 'kitty';
+    document.body.style.backgroundImage = "url('https://catdolls.weebly.com/uploads/1/0/9/6/109685771/texas-ragdoll-kittenberry-castle-website-cats-kittens-persian-himalayan-23_orig.jpg') ";
 
 });
 
@@ -29,14 +32,17 @@ catEl.addEventListener('click', () => {
 document.addEventListener('keydown', function(event) {
     if (event.key === 'c') {
         catSound.play();
-        h1El.textContent = 'cat';
+        h1El.textContent = 'kitty';
+        document.body.style.backgroundImage = "url('https://catdolls.weebly.com/uploads/1/0/9/6/109685771/texas-ragdoll-kittenberry-castle-website-cats-kittens-persian-himalayan-23_orig.jpg') ";
     }
     if (event.key === 'd') {
         dogSound.play();
-        h1El.textContent = 'dog';
+        h1El.textContent = 'doggie';
+        document.body.style.backgroundImage = "url('https://i0.wp.com/dailydot.com/wp-content/uploads/692/91/5f3554c793183fa4176cd2de936c83c5.jpg') ";
     }
     if (event.key === 'h') {
         horseSound.play();
-        h1El.textContent = 'horse';
+        h1El.textContent = 'horsey';
+         document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/e6/3f/ac/e63fac00637cce9fb90d67683d3b5e9c.jpg') ";
     }
 });
